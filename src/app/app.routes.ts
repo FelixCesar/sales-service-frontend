0fc48b5e11756d98e21bbfc5e681832cb475d141
felix <felixcesar2078@gmail.com>
2025-09-05 09:10:56 -0500

    feat: agregar componente Mensaje y actualizar rutas, estilos y pruebas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Mensual } from './mensual/mensual';
 import { Diario } from './diario/diario';
+import { Mensaje } from './mensaje/mensaje';
 
 
 export const routes: Routes = [
   { path: 'mensual', component: Mensual },
   { path: 'diario', component: Diario },
+  { path: 'mensaje', component: Mensaje },
   { path: '', redirectTo: 'mensual', pathMatch: 'full' }
 ];
